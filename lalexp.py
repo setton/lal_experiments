b4b0b5fed29db92fa73fe01fc257fc8732551223
Nicolas Setton <setton@adacore.com>
2018-10-16 14:08:52 -0400

    Reformat, transform the explorer into a class

--- lalexp.py
+++ lalexp.py
@@ -2,11 +2,6 @@ import os
 import libadalang as lal
 import GPS
 
-all_entities = {}
-# All entities.
-# keys: node
-# values: set of references (file, line, col)
-
 
 def loc_error(n, message):
     """emit error message at the location of n"""
@@ -17,42 +12,67 @@ def loc_error(n, message):
         message)
 
 
-def execute():
-    project = GPS.Project.root()
-    project_file = project.file().name()
-    provider = lal.UnitProvider.for_project(project_file)
+class LALElim(object):
 
-    ctx = lal.AnalysisContext(unit_provider=provider)
+    def __init__(self, project):
+        """Look in the given project for unused entities"""
 
-    mains = project.get_attribute_as_list('main')
+        project_file = project.file().name()
+        provider = lal.UnitProvider.for_project(project_file)
+        self.ctx = lal.AnalysisContext(unit_provider=provider)
 
-    success_count = 0
-    fail_count = 0
+        self.all_entities = {}
+        # All entities.
+        # keys: node
+        # values: set of references (file, line, col)
 
-    for source in project.sources(recursive=True):
-        if source.language().lower() != "ada":
-            continue
+        # TODO: process only the closure of mains?
+        # mains = project.get_attribute_as_list('main')
 
-        filename = source.name()
+        self.success_count = 0
+        self.fail_count = 0
 
-        unit = ctx.get_from_file(filename)
+        for source in project.sources(recursive=True):
+            if source.language().lower() != "ada":
+                continue
+
+            filename = source.name()
+            self.inspect_file(filename)
+
+        print "nodes successfully xref'ed: {}".format(self.success_count)
+        print "nodes not xref'ed: {}".format(self.fail_count)
+
+        # OK now do something
+
+        for k in self.all_entities:
+            if not self.all_entities[k]:
+                print "{}:{}:{}: entity {} not referenced".format(
+                    os.path.basename(k.unit.filename),
+                    k.sloc_range.start.line,
+                    k.sloc_range.start.column,
+                    k.text)
+
+    def inspect_file(self, filename):
+        """Look at all the defining entities in one file"""
+
+        unit = self.ctx.get_from_file(filename)
         for d in unit.diagnostics:
             print('{}: error: {}'.format(filename, d))
         if unit.diagnostics:
-            continue
+            return
 
         unit.populate_lexical_env()
         for node in unit.root.findall(lal.Identifier):
             if node.parent.is_a(lal.DefiningName):
-                if node.parent not in all_entities:
-                    all_entities[node.parent] = set()
+                if node.parent not in self.all_entities:
+                    self.all_entities[node.parent] = set()
             else:
                 xref = None
                 try:
                     xref = node.p_xref
-                    success_count += 1
+                    self.success_count += 1
                 except Exception:
-                    fail_count += 1
+                    self.fail_count += 1
                     loc_error(node, "exception when computing xref")
 
                 if xref:
@@ -63,28 +83,20 @@ def execute():
                            node.sloc_range.start.column)
                     # print key, val
 
-                    if key in all_entities:
-                        if val in all_entities[key]:
+                    if key in self.all_entities:
+                        if val in self.all_entities[key]:
                             # This could happen for package specs
                             pass
                         else:
                             if val:
-                                all_entities[key].add(val)
+                                self.all_entities[key].add(val)
                     else:
                         if val:
-                            all_entities[key] = set([val])
+                            self.all_entities[key] = set([val])
                         else:
-                            all_entities[key] = set()
+                            self.all_entities[key] = set()
 
-    print "nodes successfully xref'ed: {}".format(success_count)
-    print "nodes not xref'ed: {}".format(fail_count)
 
-    # OK now do something
-
-    for k in all_entities:
-        if not all_entities[k]:
-            print "{}:{}:{}: entity {} not referenced".format(
-                os.path.basename(k.unit.filename),
-                k.sloc_range.start.line,
-                k.sloc_range.start.column,
-                k.text)
+def execute():
+    project = GPS.Project.root()
+    LALElim(project)
